fb5a33cc28ee9dee45ddba89756b33460a50fc4d
Yovalice Roman <yovalice@me.com>
2015-12-07 19:27:29 -0400

    change home to index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('public/home.html');
+  response.render('public/index.html');
 });
 
 app.listen(app.get('port'), function() {
