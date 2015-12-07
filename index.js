8d6bb0ad68fd5373318046e55636baacb65f0823
Yovalice Roman <yovalice@me.com>
2015-12-07 19:19:42 -0400

    Change  default route from index to home

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('public/index.html');
+  response.render('public/home.html');
 });
 
 app.listen(app.get('port'), function() {
